88149c729031e387d8427876ce08f8130a05ef57
Moatkon <moatkon@gmail.com>
2025-06-26 11:03:14 +0800

    moatkon.com commit 1750906994

--- astro.config.mjs
+++ astro.config.mjs
@@ -587,6 +587,7 @@ export default defineConfig({
       items: [
         {label: 'AI',   link: '/ai'},
         {label: 'Dify',   link: '/ai/dify'},
+        {label: 'Gemini Cli',   link: '/ai/gemini/cli'},
         // {label: 'AI时代下，程序员的焦虑与出路',link: '/ai/ai-era-programmer-anxiety',badge: {text: 'Grok3',variant: 'default'}},
       ]
     },
